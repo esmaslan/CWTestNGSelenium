1e2023798f564f218e3d2327cfe70240e2acdc3b
Esma Aslan <e3s3m3a3@gmail.com>
2022-12-29 23:45:39 +0300

    ek

---
+++ src/test/java/Clarusway/homework/SoftAssertHomework02.java
@@ -0,0 +1,11 @@
+package Clarusway.homework;
+
+public class SoftAssertHomework02 {
+    //Test Case2: Negative Username Test
+    //Open page https://practicetestautomation.com/practice-test-login/
+    //Type username incorrectUser into Username field.
+    //Type password Password123 into Password field.
+    //Puch Submit button.
+    //Verify error message is displayed.
+    //Verify error message text is Your username is invalid!
+}
---
+++ src/test/java/Clarusway/homework/SoftAssertHomework03.java
@@ -0,0 +1,11 @@
+package Clarusway.homework;
+
+public class SoftAssertHomework03 {
+    //Test Case3: Negative Password Test
+    //Open page https://practicetestautomation.com/practice-test-login/
+    //Type username student into Username field.
+    //Type password incorrectPassword into Password field.
+    //Puch Submit button.
+    //Verify error message is displayed.
+    //Verify error message text is Your password is invalid!
+}
